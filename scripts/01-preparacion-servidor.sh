8ffebcd5f396476e3407ab351659a39fec4e5fff
jdmonge10 <156466752+jdmonge10@users.noreply.github.com>
2026-04-19 22:34:28 +0200

    Update server preparation script for graphical environment

--- scripts/01-preparacion-servidor.sh
+++ scripts/01-preparacion-servidor.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Actualización e instalación de componentes
-sudo apt update && sudo apt upgrade -y
+# Fase 01: Actualización e Instalación del Entorno Gráfico
+echo "Actualizando repositorios..."
+sudo apt update -y
+echo "Instalando XFCE4 y componentes adicionales..."
 sudo apt install -y xfce4 xfce4-goodies tightvncserver
-echo "Instalación completada."
+echo "Proceso finalizado."
